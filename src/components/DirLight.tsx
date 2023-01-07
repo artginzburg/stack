cc857cad53ca2d138d77a48b724df27741dc1ce0
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-01-07 21:05:57 +0400

    fix shadows and lighting to almost match the original game

--- src/components/DirLight.tsx
+++ src/components/DirLight.tsx
@@ -1,25 +1,42 @@
-import { useMemo } from 'react';
-import { Vector3 } from 'three';
+import { useThree } from '@react-three/fiber';
+import { useState } from 'react';
+import { Object3D } from 'three';
 
-export function DirLight() {
-  const d = 50;
+import type { PreviousTile } from './types';
 
-  const dirLightPosition = useMemo(() => {
-    const vector = new Vector3(-1, 1.75, -0.5);
-    vector.multiplyScalar(30);
-    return vector;
-  }, []);
+export function DirLight({ previousTile }: { previousTile: PreviousTile }) {
+  //* Uncomment the ref (also lower in code) and the helper to debug lighting.
+  // const lightCameraRef = useRef<OrthographicCamera>(null);
+  // useHelper(lightCameraRef as any, CameraHelper);
+
+  const d = 150;
+
+  const camera = useThree((state) => state.camera);
+
+  const [lightTarget] = useState(() => new Object3D());
 
   return (
-    <directionalLight
-      color={'hsl(0.1, 10%, 95%)'}
-      intensity={1}
-      castShadow
-      position={dirLightPosition}
-      shadow-mapSize={[2048, 2048]}
-      shadow-bias={-0.0001}
+    <group
+      position-y={Math.max(camera.position.y - 200, previousTile.position.y)} // using "previous tile position y" as minimum to not move camera on game end, when the scene zooms out.
     >
-      <orthographicCamera attach="shadow-camera" args={[-d, d, d, -d]} far={3500} />
-    </directionalLight>
+      <directionalLight
+        color={'hsl(0.1, 10%, 95%)'}
+        intensity={1}
+        castShadow
+        position={[-50, 50 + 10, 50]}
+        shadow-mapSize={[2048, 2048]}
+        // shadow-bias={-0.0001} // this caused weird artifacts
+        target={lightTarget}
+      >
+        <orthographicCamera
+          // ref={lightCameraRef}
+          attach="shadow-camera"
+          args={[-d, d, d, -d]}
+          far={500}
+        />
+      </directionalLight>
+      <primitive object={lightTarget} position={[0, 0, 0]} />
+      <directionalLight color="white" intensity={0.8} position={[-50, 50 + 30, 50]} />
+    </group>
   );
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -190,8 +190,8 @@ export function Game({ autoplay }: { autoplay?: boolean }) {
         shadows="basic"
       >
         <CameraController previousTile={previousTile} isEnded={isEnded} />
-        <ambientLight color="#ccc" intensity={0.4} />
-        <DirLight />
+        <ambientLight color="#ccc" intensity={0.9} />
+        <DirLight previousTile={previousTile} />
         {isStarted && !isEnded && (
           <MovingTile
             index={index}
