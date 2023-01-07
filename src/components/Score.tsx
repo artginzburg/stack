9b24871d32d05042619bb60330c44dbd76da1ffe
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-01-07 21:30:14 +0400

    fix Score text style

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,13 +1,16 @@
+import { sharedStyleProps } from './Greeting';
+
 export function Score({ index }: { index: number }) {
   return (
     <div
       style={{
-        textAlign: 'center',
-        color: 'white',
+        ...sharedStyleProps,
+        fontWeight: 200,
+        letterSpacing: 0,
+        fontStretch: '100%',
+
         fontSize: '5rem',
-        position: 'fixed',
         top: '2rem',
-        width: '100%',
       }}
     >
       {index || ''}
