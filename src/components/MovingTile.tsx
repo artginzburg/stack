artginzburg/stack

commit 3f92028441e7412473317ab38a25382ca446abc3

make side offset of Moving Tile match the original game

src/components/MovingTile.tsx
@@ -57,12 +57,15 @@ export function MovingTile({
       }
     }
 
-    if (Math.abs(mesh.position[axis]) >= 100) {
+    const sideOffset = startOffset / 4;
+    const maxSidePosition = startOffset + sideOffset;
+
+    if (Math.abs(mesh.position[axis]) >= maxSidePosition) {
       setDirection((prev) => -prev);
       // Is this `mesh.position.clamp` call necessary? Seems to work the same way without it.
       mesh.position.clamp(
-        new Vector3(-100, Number.NEGATIVE_INFINITY, -100),
-        new Vector3(100, Number.POSITIVE_INFINITY, 100),
+        new Vector3(-maxSidePosition, Number.NEGATIVE_INFINITY, -maxSidePosition),
+        new Vector3(maxSidePosition, Number.POSITIVE_INFINITY, maxSidePosition),
       );
     }
   });

commit 8cba33422c8e004508c7b65d512f8d11d66ef8da

fix Moving Tile starting in opposite direction
broke it in the previous commit (3f92028)

src/components/MovingTile.tsx
@@ -20,7 +20,8 @@ export function MovingTile({
   lastCube: TileProps | undefined;
 }) {
   const height = 10;
-  const startOffset = 100;
+  const defaultTileSize = 100;
+  const startOffset = defaultTileSize + defaultTileSize / 4;
   const [direction, setDirection] = useState(-1);
 
   useFrame((state, delta) => {
@@ -57,15 +58,12 @@ export function MovingTile({
       }
     }
 
-    const sideOffset = startOffset / 4;
-    const maxSidePosition = startOffset + sideOffset;
-
-    if (Math.abs(mesh.position[axis]) >= maxSidePosition) {
+    if (Math.abs(mesh.position[axis]) >= startOffset) {
       setDirection((prev) => -prev);
       // Is this `mesh.position.clamp` call necessary? Seems to work the same way without it.
       mesh.position.clamp(
-        new Vector3(-maxSidePosition, Number.NEGATIVE_INFINITY, -maxSidePosition),
-        new Vector3(maxSidePosition, Number.POSITIVE_INFINITY, maxSidePosition),
+        new Vector3(-startOffset, Number.NEGATIVE_INFINITY, -startOffset),
+        new Vector3(startOffset, Number.POSITIVE_INFINITY, startOffset),
       );
     }
   });
@@ -87,14 +85,7 @@ export function MovingTile({
       index * height,
       !even ? startOffset : mesh.position.z,
     );
-  }, [
-    index,
-    movingTileMeshRef,
-    previousTile.position.x,
-    previousTile.position.z,
-    previousTile.size.x,
-    previousTile.size.y,
-  ]);
+  }, [index, movingTileMeshRef, previousTile.position.x, previousTile.position.z, startOffset]);
 
   return (
     <mesh ref={movingTileMeshRef} castShadow>