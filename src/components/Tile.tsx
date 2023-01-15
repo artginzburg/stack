503f5e4394e792ccac8235a6e9e550986fdbf796
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-01-15 09:47:12 +0400

    Runtime test for size and position mismatch for Tile (#1)

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,4 +1,5 @@
 import { Triplet, useBox } from '@react-three/cannon';
+import { useState } from 'react';
 import { Mesh, Vector2, Vector3 } from 'three';
 
 import { getTileColor } from '../shared/colors';
@@ -10,6 +11,12 @@ export interface TileProps {
   index: number;
 }
 export function ReactTile({ position, size, index }: TileProps) {
+  if (process.env.NODE_ENV !== 'production') {
+    // I know for sure that NODE_ENV is never going to change during one run of the game script, so it's OK to have a conditional hook:
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useRunDevOnlyTestsForTile({ position, size, index });
+  }
+
   const boxArgs: Triplet = [size.x, config.tileHeight, size.y];
 
   const [ref] = useBox<Mesh>(() => ({
@@ -24,4 +31,35 @@ export function ReactTile({ position, size, index }: TileProps) {
       <boxGeometry args={boxArgs} />
     </mesh>
   );
+}
+
+/** @todo exclude tests into a different file. */
+function useRunDevOnlyTestsForTile({ index, ...tileProps }: TileProps) {
+  const [testsAlreadyRan, setTestsAlreadyRan] = useState({
+    testTilePosition: false,
+  });
+
+  if (testsAlreadyRan['testTilePosition']) return;
+  try {
+    testTilePosition(tileProps);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Warning at Tile ${index}`, error.message, error.cause);
+    }
+    setTestsAlreadyRan((prev) => ({ ...prev, testTilePosition: true }));
+  }
+}
+
+/** This test is necessary because the new "Tile Growing" feature (a.k.a. "combo") makes a perfect example of position miscalculation. */
+function testTilePosition({ size, position }: Pick<TileProps, 'position' | 'size'>) {
+  if (size.x === 100 && size.y === 100) {
+    if (position.x !== 0 || position.z !== 0) {
+      throw new Error('Tile size and position mismatch', {
+        cause: {
+          expects: 'tile position to be 0 when size is 100',
+          position,
+        },
+      });
+    }
+  }
 }
