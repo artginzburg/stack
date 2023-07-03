d0c0079cbf209fdb83ed3264ccec8e5362dd6b5a
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-07-03 09:18:54 +0400

    Refactor PlaneBorder

--- src/components/PlaneBorder.tsx
+++ src/components/PlaneBorder.tsx
@@ -12,29 +12,36 @@ export function PlaneBorder({
   const width = size?.[0]!;
   const height = size?.[1]!;
 
+  const argsForLeftAndRightPlanes: Parameters<typeof Plane>[0]['args'] = [
+    width + borderWidth * 2,
+    borderWidth,
+  ];
+  const argsForTopAndBottomPlanes: Parameters<typeof Plane>[0]['args'] = [borderWidth, height];
+
+  /** Maybe it's actually right plane, not sure. */
+  const positionZOfLeftPlane = height / 2 + borderWidth / 2;
+  /** Maybe it's actually bottom plane, not sure. */
+  const positionXOfTopPlane = width / 2 + borderWidth / 2;
+
   return (
     <group position={positionAsTuple}>
+      <Plane {...props} args={argsForLeftAndRightPlanes} position={[0, 0, positionZOfLeftPlane]}>
+        {children}
+      </Plane>
       <Plane
         {...props}
-        args={[width + borderWidth * 2, borderWidth]}
-        position={[0, 0, height / 2 + borderWidth / 2]}
+        args={argsForLeftAndRightPlanes}
+        position={[0, 0, positionZOfLeftPlane * -1]}
       >
         {children}
       </Plane>
-      <Plane
-        {...props}
-        args={[width + borderWidth * 2, borderWidth]}
-        position={[0, 0, -height / 2 - borderWidth / 2]}
-      >
-        {children}
-      </Plane>
-      <Plane {...props} args={[borderWidth, height]} position={[width / 2 + borderWidth / 2, 0, 0]}>
+      <Plane {...props} args={argsForTopAndBottomPlanes} position={[positionXOfTopPlane, 0, 0]}>
         {children}
       </Plane>
       <Plane
         {...props}
-        args={[borderWidth, height]}
-        position={[-width / 2 - borderWidth / 2, 0, 0]}
+        args={argsForTopAndBottomPlanes}
+        position={[positionXOfTopPlane * -1, 0, 0]}
       >
         {children}
       </Plane>
